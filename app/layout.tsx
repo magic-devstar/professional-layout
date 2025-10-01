6f404729c3dd2432d5bf6613688310e956e5f1bc
Dennis <dennisbasuki.dev@gmail.com>
2025-10-01 02:08:48 -0700

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-export default function RootLayout({
+import '@/app/ui/global.css'
+
+export default function RootLayout ({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={'${inter.className} antialiased'}>{children}</body>
     </html>
   );
 }
